bd763bac1ea3ce5306870b3ed30ad5471cf72a68
RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-07-06 21:15:45 +0530

    Update song.py

--- DaisyXMusic/modules/song.py
+++ DaisyXMusic/modules/song.py
@@ -76,7 +76,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by DaisyXMusic**"
+        rep = "**🎵 Uploaded by  🖤 ᴀʀᴄʜᴀɴɢᴇʟ ᴍɪᴄʜᴀᴇʟ 🖤**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
